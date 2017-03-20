eca6deef3ebada6590ee0810b3444c2e95227485
Đào Nguyên Anh <nguyenanh04.neu@gmail.com>
2017-03-20 14:46:33 +0700

    create setting activity

---
+++ app/src/main/java/com/example/administrator/dap_clone/ConstantValues.java
@@ -0,0 +1,9 @@
+package com.example.administrator.dap_clone;
+
+/**
+ * Created by Administrator on 03/20/2017.
+ */
+
+public class ConstantValues {
+	public final static String setting_number_thread_download = "number_thread_download";
+}
--- app/src/main/java/com/example/administrator/dap_clone/MainActivity.java
+++ app/src/main/java/com/example/administrator/dap_clone/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.administrator.dap_clone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.example.administrator.dap_clone.SettingsManager.SettingsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.activity_main_menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.menu_settings:
+				startActivity(new Intent(this, SettingsActivity.class));
+				break;
+			default:
+				break;
+		}
+		return true;
 	}
 }
